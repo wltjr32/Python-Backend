a6c59cd0ee233d820d992ad82d1121ed483696db
wltjr32 <fcjs9@naver.com>
2024-06-21 15:26:18 +0900

    MiniSNS_Signup

--- miniSNS/app.py
+++ miniSNS/app.py
@@ -1,7 +1,24 @@
-from flask import Flask, jsonify, request
+from flask import Flask,jsonify,request,render_template
+
+app = Flask(__name__)
 app.users = {}
 app.posts = []
 app.idCnt = 1
+
+@app.route('/')
+def root():
+    return """
+    <h1>miniSNS</h1>
+    <div>
+        <button>메인 페이지</button>
+        <button>회원 가입</button>
+        <button>포스팅</button>
+    <div>
+    """
+    
+@app.route('/main', methods=['GET'])
+def main():
+    return render_template('main.html', users=app.users)
 
 @app.route('/sign-up', methods=['POST'])
 def signUp():
@@ -76,6 +93,6 @@ def timeline(userId):
         'userId' : userId,
         'timeline' : timeline
     })
-
+    
 if __name__ == '__main__':
     app.run()
